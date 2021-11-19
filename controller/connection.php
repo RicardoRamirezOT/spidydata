5633eceafea28fdb9cc4783f9a198f51f6040977
RicardoRamirezOT <71525748+RicardoRamirezOT@users.noreply.github.com>
2021-11-19 16:31:21 -0600

    Update connection.php

--- controller/connection.php
+++ controller/connection.php
@@ -1,8 +1,8 @@
 <?php
   // Set main varibales of connection to the database
-  $srv = "127.0.0.1";
-  $usr = "root";
-  $psw = "";
+  $srv = "127.0.0.1:53956";
+  $usr = "azure";
+  $psw = "6#vWHD_$";
   $dbnm = "spidysql";
   $cnnsp = mysqli_connect($srv, $usr, $psw, $dbnm);
 ?>
